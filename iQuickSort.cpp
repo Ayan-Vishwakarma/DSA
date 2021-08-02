b1231a5a59f2ea0836d50373c302899c1259c5c4
Ayan-Vishwakarma <54640632+Ayan-Vishwakarma@users.noreply.github.com>
2021-08-02 18:08:18 +0530

    Create iQuickSort.cpp

---
+++ iQuickSort.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template<class T>
+void iQuickSort(T* array,T* index,int N)
+{
+    quicksort_procedure<T>(0,N-1,array,index);
+}
+template<class T>
+void quicksort_procedure(int l,int r,T* array,T* index)
+{   cout<<l<<' '<<r<<endl;
+    if(l>=r)
+    return;
+    int key = array[l];
+    int l1 = l+1,r1 = r;
+    while(l1<=r and array[l1]<=key)
+    l1++;
+    if(l1==r+1)
+    {
+        swap(array[l],array[r]);
+        swap(index[l],index[r]);
+        quicksort_procedure(l,r-1,array,index);
+        return ;
+    }
+    label1:
+    while(array[l1]<=key)
+    l1++;
+    while(array[r1]>key)
+    r1--;
+    if(r1+1==l1)
+    goto label2;
+    swap(array[l1],array[r1]);
+    swap(index[l1],index[r1]);
+    goto label1;
+    label2:
+    swap(array[l],array[r1]);
+    swap(index[l],index[r1]);
+        quicksort_procedure(l,r1-1,array,index);
+        quicksort_procedure(l1,r,array,index);
+        return ;
+    }
