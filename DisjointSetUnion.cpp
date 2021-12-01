c222bae1f12b8cf9f6847314dfc8af7628ccfa1a
Ayan-Vishwakarma <54640632+Ayan-Vishwakarma@users.noreply.github.com>
2021-12-01 07:52:53 +0530

    Create DisjointSetUnion.cpp

---
+++ DisjointSetUnion.cpp
@@ -0,0 +1,34 @@
+#define LIM 200
+int __parent[LIM],__rank[LIM];
+void __init__(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        __parent[i] = i;
+        __rank[i] = 1;
+    }
+}
+int __find__(int i)
+{
+    if(i == __parent[i])
+    return i;
+    return (__parent[i] = __find__(__parent[i]));
+}
+void __union__(int i,int j)
+{
+    int pj = __find__(j), pi = __find__(i);
+    if(pi == pj)
+    {
+        return ;
+    }
+    else if(__rank[pi] > __rank[pj])
+    {
+        __parent__[pj] = pi;
+        __rank__[pi] += __rank__[pj];
+    }
+    else
+    {
+        __parent__[pi] = pj;
+        __rank__[pj] += __rank__[pi];
+    }
+}
